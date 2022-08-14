593c09347d21f5734407e9072760af62484266c7
lf444 <lartin.fabien@gmail.com>
2022-08-15 01:47:18 +0200

    fix : heroku port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { computeChanceOfarrival } from "./compute";
 import { BountyHunter, Empire } from "./dto/Empire";
 import falcon from "./millennium-falcon.json";
 import { openDb } from "./db";
-const port = 8080;
+const port = process.env.PORT || 8080;
 // this is a top-level await
 (async () => {
   // open the database
